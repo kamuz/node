9aa4bac5c739cddf2a371876f977b527514c71b4
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-05-31 15:31:41 +0300

    Simple routing - Add exception

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ import http from 'http';
 const port = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    if( req.url === '/' ) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>Homepage</h1>');
-    } else if( req.url === '/about' ) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>About</h1>');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>Not Found</h1>');
+    try {
+        // Check if GET request
+        if( req.method === 'GET' ) {
+            if( req.url === '/' ) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>Homepage</h1>');
+            } else if( req.url === '/about' ) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>About</h1>');
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>Not Found</h1>');
+            }
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch( error ) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server Error');
     }
 });
 
