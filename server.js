f40e7719cef4a8f3e85c2c0d7cf80ca4187cd7df
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-05-31 15:30:29 +0300

    Simple routing

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ import http from 'http';
 const port = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    res.end('<h1>Hello, Node.js</h1>');
+    if( req.url === '/' ) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Homepage</h1>');
+    } else if( req.url === '/about' ) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>About</h1>');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>Not Found</h1>');
+    }
 });
 
 server.listen(port, () => {
