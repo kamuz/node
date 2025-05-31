19180a7abb2a8cd591e383562c545ca950990892
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-05-31 14:39:52 +0300

    HTTP Module & Create Server - Set HTTP header short version

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ import http from 'http';
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 404;
-    res.end('<h1>Hello, Node.js</h1>');
+    res.writeHead(500, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({message: 'Server Error'}));
 });
 
 server.listen(port, () => {
