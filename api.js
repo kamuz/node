0b38fbd9a195789b339f97fe066f79c85f11569b
Volodymyr Kamuz <v.kamuz@gmail.com>
2026-03-03 00:12:51 +0200

    Get user by id

--- api.js
+++ api.js
@@ -13,6 +13,21 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(users));
         res.end();
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        const id = req.url.split('/')[3];
+        // console.log(id);
+        const user = users.find((user) => user.id === parseInt(id));
+        // console.log(user);
+        if(user){
+            // res.write(JSON.stringify({ id: 1, name: 'Jonh Dou'}));
+            res.write(JSON.stringify(user));
+            res.end();
+        } else {
+            res.statusCode = 404;
+            res.write(JSON.stringify({message: 'User not found'}));
+            res.end();
+        }
     } else {
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
