41fc8d6e00172e7f14de201a8a819b152793b639
Volodymyr Kamuz <v.kamuz@gmail.com>
2026-08-28 11:09:36 +0300

    File System module - Read file, sync version

--- fsDemo.js
+++ fsDemo.js
@@ -6,3 +6,7 @@ fs.readFile('./test.txt', 'utf8', (err, data) => {
 	if(err) throw err;
 	console.log(data);
 });
+
+// Read file - synchronous version
+const data = fs.readFileSync('./test.txt', 'utf8');
+console.log(data);
