7fd0640a04561bb264fe2ba75cc899e052deb6ba
Volodymyr Kamuz <v.kamuz@gmail.com>
2026-08-31 19:33:10 +0300

    OS Module

---
+++ osDemo.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+// User info
+console.log(os.userInfo());
+
+// Total memory
+console.log(os.totalmem());
+
+// Free memory
+console.log(os.freemem());
+
+// CPU
+console.log(os.cpus());
